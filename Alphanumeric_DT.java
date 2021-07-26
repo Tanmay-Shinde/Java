7e30c47c6bc984be314bca44ffa3fdd171468491
Tanmay-Shinde <79076214+Tanmay-Shinde@users.noreply.github.com>
2021-07-26 15:12:29 +0530

    Add files via upload

---
+++ Alphanumeric_DT.java
@@ -0,0 +1,25 @@
+public class Alphanumeric_DT
+{
+    void main()
+    {
+        int m=9;
+        System.out.println(m);
+        
+        char c='9';
+        System.out.println(c);
+        char ch = '#';
+        System.out.println(ch);
+        char x = 97;
+        System.out.println(x);
+        
+        String p="Hello";
+        System.out.println(p);
+        String s="234";
+        System.out.println(s);
+        
+        boolean k=true;
+        System.out.println(k);
+        boolean f=false;
+        System.out.println(f);
+    }
+}
